ea9e9d0a2ee2d33d3b137713acba0a845b493d7c
nobody <nobody@2d7181c2-f438-4696-8f62-672025efbd90>
2005-01-11 19:53:15 +0000

    This commit was manufactured by cvs2svn to create tag 'unfs3-0-9-12'.

---
+++ tags/unfs3-0-9-12/unfs3/daemon.h
@@ -0,0 +1,40 @@
+/*
+ * UNFS3 server framework
+ * (C) 2004, Pascal Schmidt <der.eremit@email.de>
+ * see file LICENSE for license details
+ */
+
+#ifndef UNFS3_DAEMON_H
+#define UNFS3_DAEMON_H
+
+/* exit status for internal errors */
+#define CRISIS	99
+
+/* HP-UX does not have seteuid() and setegid() */
+#if HAVE_SETEUID == 0 && HAVE_SETRESUID == 1
+#define seteuid(u) setresuid(-1, u, -1)
+#endif
+#if HAVE_SETEGID == 0 && HAVE_SETRESGID == 1
+#define setegid(g) setresgid(-1, g, -1)
+#endif
+
+/* error handling */
+void daemon_exit(int);
+void logmsg(int, const char *, ...);
+
+/* remote address */
+struct in_addr get_remote(struct svc_req *);
+
+/* write verifier */
+extern writeverf3 wverf;
+
+/* options */
+extern int	opt_expire_writers;
+extern int	opt_detach;
+extern char	*opt_exports;
+extern int	opt_cluster;
+extern char	*opt_cluster_path;
+extern int	opt_singleuser;
+extern int	opt_brute_force;
+
+#endif
