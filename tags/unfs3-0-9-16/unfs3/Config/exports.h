4622499fdb3054ad2f6f5a12051c7b6829363118
nobody <nobody@2d7181c2-f438-4696-8f62-672025efbd90>
2006-11-04 18:29:40 +0000

    This commit was manufactured by cvs2svn to create tag 'unfs3-0-9-16'.

---
+++ tags/unfs3-0-9-16/unfs3/Config/exports.h
@@ -0,0 +1,33 @@
+/*
+ * UNFS3 export controls
+ * (C) 2003, Pascal Schmidt
+ * see file LICENSE for license details
+ */
+
+#ifndef UNFS3_EXPORTS_H
+#define UNFS3_EXPORTS_H
+
+#define OPT_NO_ROOT_SQUASH	1
+#define OPT_ALL_SQUASH		2
+#define OPT_RW			4
+#define OPT_REMOVABLE		8
+#define OPT_INSECURE		16
+
+#define PASSWORD_MAXLEN   64
+
+extern exports	exports_nfslist;
+/* Options cache */
+extern int	exports_opts;
+extern uint32   export_password_hash;
+
+extern unsigned char password[PASSWORD_MAXLEN+1];
+
+void		exports_parse(void);
+int		exports_options(const char *path, struct svc_req *rqstp, char **password, uint32 *fsid);
+int             export_point(const char *path);
+char            *export_point_from_fsid(uint32 fsid, time_t **last_mtime, uint32 **dir_hash);
+nfsstat3	exports_compat(const char *path, struct svc_req *rqstp);
+nfsstat3	exports_rw(void);
+uint32          fnv1a_32(const char *str, uint32 hval);
+
+#endif
